fb5a6b2a4088fb7dd4cbdebc3cbc89f6bfd05ea6
Chamath Senanayake <2020cs171@stu.ucsc.cmb.ac.lk>
2023-07-07 00:30:29 +0530

    Added offers button

--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -1,23 +1,45 @@
 package com.example.app1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val greetingTextView = findViewById<TextView>(R.id.tv_message)
         val inputField = findViewById<EditText>(R.id.etName)
         val btn = findViewById<Button>(R.id.btnSubmit )
+        val btnOffers = findViewById<Button>(R.id.btnOffers)
         btn.setOnClickListener{
             val enteredName = inputField.text.toString()
-            val message = "Welcome $enteredName"
-            greetingTextView.text = message
-            inputField.text.clear()
+            if(enteredName==""){
+                btnOffers.visibility = INVISIBLE
+                greetingTextView.text=""
+                Toast.makeText(applicationContext,
+                    "Please enter your name",
+                    Toast.LENGTH_SHORT).show()
+            }else{
+                val message = "Welcome $enteredName"
+                greetingTextView.text = message
+                inputField.text.clear()
+                btnOffers.visibility = VISIBLE
+            }
+        }
+        btnOffers.setOnClickListener {
+            greetingTextView.text = "Offers wada"
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
